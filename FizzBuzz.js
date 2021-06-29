cc8a0eeae1d9d5cfc31e2814052e7bf66993773b
AgataS23 <agata.stoch@gmail.com>
2021-06-29 07:59:44 +0200

    solution FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -17,3 +17,19 @@
 
 // Input: n = 15
 // Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+const fizzBuzz = function (n) {
+  const arr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else if (i % 5 === 0) {
+      arr.push("Buzz");
+    } else {
+      arr.push(i.toString());
+    }
+  }
+  return arr;
+};
