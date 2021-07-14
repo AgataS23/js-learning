5395223355896932e2cdfd7fa7257c03957974de
AgataS23 <agata.stoch@gmail.com>
2021-07-14 10:39:14 +0200

    feat: map-exercise.js

---
+++ map-exercise.js
@@ -0,0 +1,5 @@
+// Napisz funkcję toFullNames, która zamienia listę użytkowników na listę ich
+// pełnych imion i nazwisk w formacie "{imię} {drugie imię} {nazwisko}",
+// lub "{imię} {nazwisko}" jeśli nie ma drugiego imienia. Załóż, że użytkownicy
+// mają właściwości firstName, lastName o typie string, oraz secondName
+// typu string lub undefined.
