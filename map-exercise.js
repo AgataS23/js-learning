f6e5e8626d6ed4a5d6e9ba1a9035786cda002bfd
AgataS23 <agata.stoch@gmail.com>
2021-07-14 10:45:37 +0200

    feat: solution of map-exercise.js

--- map-exercise.js
+++ map-exercise.js
@@ -3,3 +3,28 @@
 // lub "{imię} {nazwisko}" jeśli nie ma drugiego imienia. Załóż, że użytkownicy
 // mają właściwości firstName, lastName o typie string, oraz secondName
 // typu string lub undefined.
+// solution:
+function toFullNamesUsers(users) {
+  return users.map(function (user) {
+    if (user.secondName) {
+      return `${user.firstName} ${user.secondName} ${user.lastName}`;
+    } else {
+      return `${user.firstName} ${user.lastName}`;
+    }
+  });
+}
+const users = [
+  { firstName: "Turanga", lastName: "Leela" },
+  { firstName: "Amy", lastName: "Wong" },
+  {
+    firstName: "Philip",
+    secondName: "Jay",
+    lastName: "Fry",
+  },
+  {
+    firstName: "Bender",
+    secondName: "Bending",
+    lastName: "Rodríguez",
+  },
+];
+toFullNamesUsers(users);
