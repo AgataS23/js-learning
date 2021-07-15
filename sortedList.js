3faded6e83737af2a82f6c431aaabf755910b6ee
AgataS23 <agata.stoch@gmail.com>
2021-07-15 07:16:37 +0200

    feat: exercise sortedList.js

---
+++ sortedList.js
@@ -0,0 +1,7 @@
+// Zaimplementuj funkcję przyjmującą jako argument tablicę dowolnych elementów.
+// W przypadku gdy jest to string lub liczba, element taki zostaje dodany do tablicy wyjściowej.
+// Pozostałe typy nie są uwzględniane w tablicy wyjściowej. Tak utworzoną tablicę należyposortować
+// w kolejności: cyfry/liczby od najmniejszej do największej, litery/słowa od Z do A.
+
+// input: [12, 3, 'das', 'pol', {}, null]
+// output: [3, 12, 'pol', 'das']
